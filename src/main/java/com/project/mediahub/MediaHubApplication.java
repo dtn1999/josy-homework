c24e33d72a6072538cc1bc19afc80fe43a0f7535
dtn1999 <tchekamboudanyls@gmail.com>
2024-02-12 12:20:07 +0100

    initial commit. Add the index html file

--- src/main/java/com/project/mediahub/MediaHubApplication.java
+++ src/main/java/com/project/mediahub/MediaHubApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JoseyApplication {
+public class MediaHubApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JoseyApplication.class, args);
+        SpringApplication.run(MediaHubApplication.class, args);
     }
 
 }
