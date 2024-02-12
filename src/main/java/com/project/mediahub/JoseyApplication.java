285aa6a0ae21a194300c8c4680b4acb36316dc5a
dtn1999 <tchekamboudanyls@gmail.com>
2024-02-12 12:13:11 +0100

    initial commit

---
+++ src/main/java/com/project/mediahub/JoseyApplication.java
@@ -0,0 +1,13 @@
+package com.project.mediahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoseyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoseyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/mediahub/JoseyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mediahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoseyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
